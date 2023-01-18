eb2f98dd6a6fbaf95879d68d9765c9ddbfcce680
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2023-01-18 21:20:55 +0100

    Responsive Navbar component added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false)
 
   return (
-    <div>
+    
       <nav className="w-full flex py-6 justify-between items-center navbar">
-        <img src={logo} alt="logo" className="w-[400px] h-[102px]" />
+        <img src={logo} alt="logo" className="w-[124px] h-[32px]" />
 
         <ul className="list-none sm:flex hidden justify-end items-center flex-1">
           {navLinks.map((nav, index) => (
             <li
               key={nav.id}
-              className={`font-poppins font-normal cursor-pointer text-[34px]
+              className={`font-poppins font-normal cursor-pointer text-[16px]
              ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}
             >
               <a href={`#${nav.id}`}>{nav.title}</a>
@@ -50,7 +50,6 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-    </div>
   )
 }
 
